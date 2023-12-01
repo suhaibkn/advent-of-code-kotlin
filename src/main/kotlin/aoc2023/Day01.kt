aa5cc3f455666033de9b2dc4fb4e590e6e24c4fe
Suhaib Khan <suhaib96@gmail.com>
2023-12-01 18:58:23 +0530

    feat(day-01): add solution for 1st December, 2023

---
+++ src/main/kotlin/aoc2023/Day01.kt
@@ -0,0 +1,46 @@
+package aoc2023
+
+import aoc.helper.getInputFor2023
+import java.util.stream.Stream
+
+fun main() {
+    print("Output for Part 1: ")
+    part1()
+
+    print("Output for Part 2: ")
+    part2()
+}
+
+fun part1() {
+    getInputForDay01()
+        .map {
+            it.split("").filter { e -> (0..9).contains(e.toIntOrNull()) }
+        }
+        .mapToInt { (it.first() + it.last()).toInt() }
+        .sum()
+        .run(::println)
+}
+
+fun part2() {
+    getInputForDay01()
+        .map {
+            it
+                .replace("one", "one1one")
+                .replace("two", "two2two")
+                .replace("three", "three3three")
+                .replace("four", "four4four")
+                .replace("five", "five5five")
+                .replace("six", "six6six")
+                .replace("seven", "seven7seven")
+                .replace("eight", "eight8eight")
+                .replace("nine", "nine9nine")
+        }
+        .map {
+            it.split("").filter { e -> (0..9).contains(e.toIntOrNull()) }
+        }
+        .mapToInt { (it.first() + it.last()).toInt() }
+        .sum()
+        .run(::println)
+}
+
+private fun getInputForDay01(): Stream<String> = getInputFor2023("01").lines()
